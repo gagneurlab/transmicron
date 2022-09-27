50818332982df4def05c0be2e7697454ff99ed3a
AtaJadidAhari <atajadidahari@gmail.com>
2022-09-28 00:53:49 +0200

    renamed variables

--- run.py
+++ run.py
@@ -12,14 +12,14 @@ def get_args() -> Namespace:
     parser.add_argument("--datasets", default='DLBCLPB', type=str,)
     parser.add_argument("--insertionFile", default='Input/TestInsertionBed/DLBCLPB.BED', type=str,)
     parser.add_argument("--transposonSystem", default='PB', type=str,)
-    parser.add_argument("--mutagenesis_method", default='predefinedFeatures', type=str,)
+    parser.add_argument("--mutagenesisMethod", default='predefinedFeatures', type=str,)
     parser.add_argument("--annotation", default='genes', type=str,)
     parser.add_argument("--CustomAnnotation", default='Input/testAnnot.BED', type=str,)
-    parser.add_argument("--multest_correction", default='bonferroni', type=str,)
+    parser.add_argument("--multestCorrection", default='bonferroni', type=str,)
     parser.add_argument("--promoters", default=0, type=int, )
-    parser.add_argument("--snakemake_rule", default="-n", type=str)
+    parser.add_argument("--snakemakeRule", default="-n", type=str)
     parser.add_argument("--customFeatures", default="Input/FeatureBeds/DNASEmesc_SRX1452763.05.bed,Input/FeatureBeds/DNASEmesc_SRX1452763.05.bed", type=str) 
-    parser.add_argument("--output_dir", default="./Output", type=str,)
+    parser.add_argument("--outputDir", default="./Output", type=str,)
     return parser.parse_args()
 
 
@@ -28,8 +28,8 @@ args = vars(args)
 args['datasets'] = args['datasets'].split(',')
 args['insertionFile'] = args['insertionFile'].split(',')
 args['transposonSystem'] = args['transposonSystem'].split(',')
-args['multest_correction'] = args['multest_correction'].split(',')
-args['mutagenesis_method'] = args['mutagenesis_method'].split(',')
+args['multest_correction'] = args['multestCorrection'].split(',')
+args['mutagenesis_method'] = args['mutagenesisMethod'].split(',')
 args['annotation'] = args['annotation'].split(',')
 args['customFeatures'] = args['customFeatures'].split(',')
 
@@ -40,4 +40,4 @@ with open('./user_config.yaml', 'w') as file:
 
 
 #os.system("./run_slurm.sh" + " " + args['snakemake_rule'])
-os.system("snakemake " + args['snakemake_rule'])
+os.system("snakemake " + args['snakemakeRule'])
