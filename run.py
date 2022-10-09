101424e548824c8502760e4e3491206dcccbc3ba
Carl Bredthauer <bredthau@ouga05.cmm.in.tum.de>
2022-10-09 21:56:42 +0200

    fix annotation.endswith

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def get_args() -> Namespace:
     parser.add_argument("--customAnnotation", default='Input/testAnnot.BED', type=str,)
     parser.add_argument("--multestCorrection", default='bonferroni', type=str,)
     #parser.add_argument("--promoters", default=0, type=int, )  ## not implemented yet
-    parser.add_argument("--snakemakeRule", default="-n", type=str)
+    parser.add_argument("--snakemakeRule", type=str)
     parser.add_argument("--customFeatures", default="Input/FeatureBeds/DNASEmesc_SRX1452763.05.bed,Input/FeatureBeds/DNASEmesc_SRX1452763.05.bed", type=str) 
     parser.add_argument("--outputDir", default="./Output", type=str,)
     return parser.parse_args()
